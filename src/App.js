869fa0f62a617811a02aef94d97c9bc1f6ad2e16
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2021-09-03 20:55:48 +0430

    add page not found

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React from "react";
 import styled from "styled-components";
 import Header from "./component/navbar/Header";
 import "bootstrap/dist/css/bootstrap.css";
+import { Route, Switch } from "react-router-dom";
+import Homepage from "./component/Homepage";
 
-import Cards from "./component/Cards";
+import Aboutus from "./component/navbar/Aboutus";
+import Login from "./component/navbar/Login";
+import PageNotFound from "./component/navbar/PageNotFound";
 const Div = styled.div`
   margin: 0;
   padding: 0;
-  background-color: beige;
+  background-color: #cee5d0;
   width: 100%;
 `;
 function App() {
   return (
     <Div>
       <Header />
-      <Cards />
+      <Switch>
+        <Route path="/" component={Homepage} exact />
+        <Route path="/Aboutus" component={Aboutus} />
+        <Route path="/Login" component={Login} />
+        <Route component={PageNotFound} />
+      </Switch>
     </Div>
   );
 }
---
+++ src/component/navbar/PageNotFound.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function PageNotFound() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>PageNotFound (:</h1>
+
+      <img
+        style={{ width: "200px" }}
+        src="https://www.rooseveltlibrary.org/wp-content/uploads/2016/10/PageNotFound.png"
+      />
+    </div>
+  );
+}
+
+export default PageNotFound;
